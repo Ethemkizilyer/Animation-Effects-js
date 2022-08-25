0d8dce60759ef01d2687d26f8f1dc19e921e6fed
Ethem <ethemkizilyer3546@gmail.com>
2022-08-25 19:18:03 +0300

    initialcommit

---
+++ app.js
@@ -0,0 +1,21 @@
+function circle(){
+    let amount =100;
+    let body = document.querySelector("body");
+    let i =0;
+    while(i < amount){
+        let element = document.createElement('i');
+        let posX = Math.floor(Math.random() * window.innerWidth);
+        let delay = Math.random() * -20;
+        let duration = Math.random() * 10;
+let size = Math.random() * 250;
+element.style.width = 10 + size + 'px';
+element.style.height = 10 + size + 'px';
+element.style.left = posX + 'px'
+element.style.animationDelay = delay + 's'
+element.style.animationDuration = 5 + duration + 's';
+element.style.boxShadow = "0 30px 50px #0024"
+        body.appendChild(element);
+        i++;
+    }
+}
+circle()
